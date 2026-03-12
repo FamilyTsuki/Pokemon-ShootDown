328ce69c53244890363af816132796c5111b2175
raphael <raphaelecerf@gmail.com>
2026-03-12 14:55:29 +0100

    ajout de pokemon pour arriver a un totale de 10 pokemon

--- simulator/src/main/java/com/pokemon/pokemonList/Bulbasaur.java
+++ simulator/src/main/java/com/pokemon/pokemonList/Bulbasaur.java
@@ -4,6 +4,6 @@ import com.pokemon.models.Pokemon;
 
 public class Bulbasaur extends Pokemon {
     public Bulbasaur() {
-        super(001, "Bulbasaur", 100, 25, 25, 35, "f", "f");
+        super(001, "Bulbasaur", 60, 10, 25, 15, "f", "f");
     }
 }
---
+++ simulator/src/main/java/com/pokemon/pokemonList/Wartortle.java
@@ -0,0 +1,9 @@
+package com.pokemon.pokemonList;
+
+import com.pokemon.models.Pokemon;
+
+public class Wartortle extends Pokemon {
+    public Wartortle() {
+        super(008, "Wartortle", 100, 25, 25, 35, "f", "f");
+    }
+}
