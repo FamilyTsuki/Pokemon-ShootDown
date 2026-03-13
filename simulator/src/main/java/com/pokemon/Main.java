7874618b9087e2c41724ff816fabc59509a0ac1f
FamilyTsuki <alban.elie590@gmail.com>
2026-03-13 13:09:48 +0100

    bug fix add attak and visuelle

--- simulator/src/main/java/com/pokemon/Main.java
+++ simulator/src/main/java/com/pokemon/Main.java
@@ -1,6 +1,13 @@
 package com.pokemon;
 
+import com.pokemon.controllers.BattleController;
+import com.pokemon.models.Pokemon;
+import com.pokemon.pokemonList.Blastoise;
+import com.pokemon.pokemonList.Charizard;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -8,14 +15,23 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        Label label = new Label("SHOOT DOWN!");
-        Scene scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.setTitle("Pokemon ShootDown");
-        stage.show();
-    }
+
+public void start(Stage stage) throws Exception {
+    
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/pokemon/views/battle_view.fxml"));
+    Parent root = loader.load();
+
+    BattleController controller = loader.getController();
+
+    Pokemon playerPkm = new Blastoise();
+    Pokemon cpuPkm = new Blastoise();
+
+    controller.setupBattle(playerPkm, cpuPkm);
+
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
 
     public static void main(String[] args) {
         launch();
--- simulator/src/main/java/com/pokemon/pokemonList/Blastoise.java
+++ simulator/src/main/java/com/pokemon/pokemonList/Blastoise.java
@@ -3,16 +3,19 @@ package com.pokemon.pokemonList;
 import com.pokemon.models.Attack;
 import com.pokemon.models.Pokemon;
 import com.pokemon.models.PokemonType;
+import com.pokemon.models.attack.Rumble;
 
 public class Blastoise extends Pokemon {
     public Blastoise() {
-        super(1,
+
+        super(9, 
                 "Blastoise",
-                60, 10,
-                25,
-                15,
-                new PokemonType[]{PokemonType.GRASS, PokemonType.POISON},
-                null,
+                79, 
+                83,
+                100,
+                78,
+                new PokemonType[]{PokemonType.WATER},
+                new Attack[]{ new Rumble() },
                 null
         );
     }
